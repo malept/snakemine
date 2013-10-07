c57c18b5e70a1f69dc62013bc0c490c569411589
Mark Lee <snakemine@lazymalevolence.com>
2013-10-06 17:43:03 -0700

    Add coverage & flake8 support; add more unit tests

--- snakemine/tests/test_issue.py
+++ snakemine/tests/test_issue.py
@@ -6,10 +6,13 @@ from unittest import TestCase
 
 
 class IssueTest(TestCase):
+    '''Assumes that the Redmine instance with the fixture data is running.'''
+
+    ALL_ISSUE_CT = 11
 
     def test_all(self):
         issues = Issue.objects.all()
-        self.assertEqual(10, len(issues))
+        self.assertEqual(self.ALL_ISSUE_CT, len(issues))
         for issue in issues:
             self.assertIsInstance(issue, Issue)
 
@@ -17,9 +20,13 @@ class IssueTest(TestCase):
         first_issue = Issue.objects.get(1)
         self.assertIsNotNone(first_issue)
 
+    def test_filter(self):
+        issues = Issue.objects.filter(assigned_to_id=2)
+        self.assertIsNotNone(issues)
+        self.assertNotEqual(self.ALL_ISSUE_CT, len(issues))
+
     def test_attrs(self):
         issue = Issue.objects.get(1)
-        self.assertIsNone(issue.parent)
         self.assertTrue(hasattr(issue, 'author'))
         self.assertEqual('John Smith', issue.author.name)
         self.assertFalse(hasattr(issue, 'assigned_to'))
@@ -32,3 +39,21 @@ class IssueTest(TestCase):
         self.assertEqual('Dave Lopper', issue2.assigned_to.name)
         self.assertTrue(hasattr(issue2, 'comments'))
         self.assertEqual(1, len(issue2.comments))
+
+    # def test_attr_project(self):
+    #     issue = Issue.objects.get(1)
+    #     project = issue.project
+    #     self.assertIsNotNone(project)
+
+    def test_attr_parent(self):
+        child = Issue.objects.get(14)
+        parent = child.parent
+        self.assertIsNotNone(parent)
+        self.assertIsInstance(parent, type(child))
+        self.assertIsNone(parent.parent)
+
+    def test_equality(self):
+        issue1 = Issue.objects.get(1)
+        issue2 = Issue.objects.get(1)
+        self.assertNotEqual(id(issue1), id(issue2))
+        self.assertEqual(issue1, issue2)
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -17,6 +17,8 @@ REDMINE_TGZ_URL=http://rubyforge.org/frs/download.php/73457/${REDMINE_TGZ}
 LOCAL_REDMINE_TGZ="$DOWNLOAD_CACHE/$REDMINE_TGZ"
 LOCAL_REDMINE_DIR="$UNZIP_DIR/$REDMINE_DIR"
 
+flake8 .
+
 if [[ -z "$NO_SETUP_NEEDED" ]]; then
     if [[ ! -f "$LOCAL_REDMINE_TGZ" ]]; then
         wget -O "$LOCAL_REDMINE_TGZ" "$REDMINE_TGZ_URL"
@@ -38,12 +40,13 @@ if [[ -z "$NO_SETUP_NEEDED" ]]; then
         rm rack-1.0.1.gem
     )
     rake generate_session_store db:migrate db:fixtures:load
-    script/runner 'Token.create!(:action => "api", :user_id => 2, :value => "1234abcd")'
+    script/runner 'Token.create!(:action => "api", :user_id => 2, :value => "1234abcd");
+Issue.create!(:parent_issue_id => 1, :subject => "parent issue test", :tracker_id => 1, :project_id => 1, :description => "Parent issue test", :author_id => 3)'
 else
     cd "$LOCAL_REDMINE_DIR"
 fi
 
 script/server --daemon --binding=127.0.0.1
 sleep 5 # make sure the stupid server is ready to accept connections
-(cd "$BASE_DIR"/.. && python -m unittest discover)
+(cd "$BASE_DIR"/.. && coverage run -m unittest discover; coverage report -m)
 kill -9 `cat tmp/pids/server.pid`
