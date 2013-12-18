ecb614f756a4a67689714e9d082faa228d6f6cf4
Mark Lee <snakemine@lazymalevolence.com>
2013-12-17 16:39:12 -0800

    tests: another attempt at fixing the rdoc error

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -29,7 +29,7 @@ REDMINE_TGZ_URL=http://rubyforge.org/frs/download.php/73457/${REDMINE_TGZ}
 REDMINE_SVN_URL=http://svn.redmine.org/redmine/tags/${REDMINE_VERSION}
 
 RACK_VERSION=1.0.1
-RAILS_VERSION=2.3.5
+RAILS_VERSION=2.3.15
 
 LOCAL_REDMINE_TGZ="$DOWNLOAD_CACHE/$REDMINE_TGZ"
 LOCAL_REDMINE_DIR="$UNZIP_DIR/$REDMINE_DIR"
@@ -57,10 +57,8 @@ unpack_gem() {
 
 # Set the correct RVM ruby (1.8.7) if RVM is installed
 if [[ $(which rvm > /dev/null) -eq 0 ]]; then
-    if [[ $(type rvm | head -n 1 | grep -o function) == "function" ]]; then
-        echo 'Loading the RVM script...'
-        [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-    fi
+    echo 'Loading the RVM script...'
+    [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
     echo 'Setting Ruby to 1.8.7...'
     rvm use 1.8.7
 fi
@@ -86,11 +84,9 @@ if [[ -z "$NO_SETUP_NEEDED" ]]; then
     perl -0 -i -pe 's@(rest_api_enabled:\s+default:) 0@$1 1@msg' config/settings.yml
     cp "$BASE_DIR"/database.yml "$LOCAL_REDMINE_DIR"/config/
     if [[ "$REDMINE_DOWNLOAD_METHOD" == "SVN" ]]; then
-        # Force install old rake
-        gem uninstall rake
-        gem install rake -v 0.8.7
         # if redmine comes from SVN, install rails too
         gem install rails -v "$RAILS_VERSION"
+        sed -i -e 's@rake/rdoctask@rdoc/task@g' "$LOCAL_REDMINE_DIR"/Rakefile
     else
         # install one gem locally
         unpack_gem rack $RACK_VERSION vendor/gems
