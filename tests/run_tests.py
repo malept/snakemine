4077e62edbc2a07ac416a2af6bc5148b7b964912
Mark Lee <snakemine@lazymalevolence.com>
2014-01-20 19:05:33 -0800

    Refactor test harness
    * Mostly uses argparse instead of environment variables
    * Broke the logic up into more functions

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -16,6 +16,11 @@
 
 from __future__ import print_function
 
+import argparse
+try:
+    import argcomplete
+except ImportError:
+    argcomplete = None
 from contextlib import contextmanager
 from flake8.engine import get_style_guide
 from flake8.main import print_report
@@ -46,27 +51,13 @@ BASE_DIR = os.path.dirname(TEST_DIR)
 DEFAULT_RM_VERSION = '1.0.4'
 
 # ENVIRONMENT
-DOWNLOAD_CACHE = os.environ.get('DOWNLOAD_CACHE', '/tmp')
-UNZIP_DIR = os.environ.get('UNZIP_DIR', '/tmp')
-RM_DOWNLOAD_METHOD = os.environ.get('REDMINE_DOWNLOAD_METHOD', 'TGZ')
-RM_VERSION = os.environ.get('REDMINE_VERSION', DEFAULT_RM_VERSION)
-RVM_NOT_SET = os.environ.get('RVM_ALREADY_SET') is None
-RM_SETUP_NEEDED = os.environ.get('NO_SETUP_NEEDED') is None
 if os.environ.get('REDMINE_PORT_RANDOM'):
     RM_PORT = randint(3000, 65535)
 else:
     RM_PORT = os.environ.get('REDMINE_PORT', '3000')
-RUN_COVERAGE = os.environ.get('RUN_COVERAGE')
 
 # REDMINE DATA
-RM_VERSION_INFO = tuple(RM_VERSION.split('.'))
-RM_DIRNAME = 'redmine-{0}'.format(RM_VERSION)
-RM_TGZ = '{0}.tar.gz'.format(RM_DIRNAME)
-RM_TGZ_URL = \
-    'http://files.rubyforge.vm.bytemark.co.uk/redmine/{0}'.format(RM_TGZ)
-RM_SVN_URL = 'http://svn.redmine.org/redmine/tags/{0}'.format(RM_VERSION)
-LOCAL_RM_TGZ = os.path.join(DOWNLOAD_CACHE, RM_TGZ)
-LOCAL_RM_DIR = os.path.join(UNZIP_DIR, RM_DIRNAME)
+RM_TGZ_FMT = 'http://files.rubyforge.vm.bytemark.co.uk/redmine/{0}'
 
 RAILS_ENV = os.environ.copy()
 RAILS_ENV['RAILS_ENV'] = 'production'
@@ -95,6 +86,35 @@ RM_DEPS = {
 # Functions
 
 
+def parse_args(prog, args):
+    parser = argparse.ArgumentParser(prog)
+    parser.add_argument('--redmine-version', default=DEFAULT_RM_VERSION,
+                        metavar='VERSION',
+                        help='The version of Redmine to test against')
+    parser.add_argument('--use-existing-redmine-install', action='store_false',
+                        default=True, dest='redmine_setup_needed',
+                        help='Use the existing Redmine install instead of '
+                             'recreating it')
+    parser.add_argument('--redmine-download-method', default='TGZ',
+                        choices=['TGZ', 'SVN'],
+                        help='The method that the Redmine code is downloaded')
+    parser.add_argument('--no-flake8', action='store_false', default=True,
+                        dest='flake8', help='Disable Flake8 check')
+    parser.add_argument('--check-rvm-ruby', action='store_true', default=False,
+                        help='Installs the proper RVM Ruby version if not '
+                             'already installed')
+    parser.add_argument('--workspace', metavar='DIR', default='/tmp',
+                        help='Directory where Redmine directory is located')
+    parser.add_argument('--download-cache', metavar='DIR', default='/tmp',
+                        help='Directory where tarballs/gems are cached')
+    parser.add_argument('--run-coverage', action='store_true', default=False,
+                        help='Run coverage.py on the testsuite')
+
+    if argcomplete:
+        argcomplete.autocomplete(parser)
+    return parser.parse_args(args)
+
+
 def run(*cmd, **kwargs):
     stdin = kwargs.pop('stdin', None)
     kwargs['stdin'] = PIPE
@@ -110,33 +130,59 @@ def run(*cmd, **kwargs):
         return proc.pid
 
 
-def rvm(*args, **kwargs):
-    cmd = 'rvm {0}'.format(' '.join(args))
-    kwargs['shell'] = True
-    return run(cmd, **kwargs)
-
-ruby_version = RM_DEPS[RM_VERSION]['ruby']
-rvm_do = partial(rvm, ruby_version, 'do')
-
-gem = partial(rvm_do, 'gem')
-gem_list = partial(gem, 'list', stdout=PIPE)
+def svn_checkout(redmine_version, target_dir):
+    url = 'http://svn.redmine.org/redmine/tags/{0}'.format(redmine_version)
+    run('svn', 'co', url, target_dir)
 
 
-def gem_unpack(name, version, unpack_dir):
-    gem_id = '{0}-{1}'.format(name, version)
-    gem_file = '{0}.gem'.format(gem_id)
-    local_gem = os.path.join(DOWNLOAD_CACHE, gem_file)
-    if not os.path.exists(local_gem):
-        print('Downloading the {0} gem...'.format(gem_id))
-        gem('fetch', name, '-v', version, cwd=DOWNLOAD_CACHE)
-    gem('unpack', local_gem, cwd=unpack_dir)
+def unpack_tarball(args, redmine_dirname):
+    tgz_basename = '{0}.tar.gz'.format(redmine_dirname)
+    tgz_url = RM_TGZ_FMT.format(tgz_basename)
+    tgz_path = os.path.join(args.download_cache, tgz_basename)
+    if not os.path.exists(tgz_path):
+        print('Downloading Redmine {0}...'.format(args.redmine_version))
+        run('wget', '-O', tgz_path, tgz_url)
+    run('tar', '-C', args.workspace, '-xf', tgz_path)
 
 
-def gem_install(*gem_args, **kwargs):
-    cmd = ['install', '--no-ri', '--no-rdoc'] + list(gem_args)
-    return gem(*cmd, **kwargs)
+class RVM(object):
+    def __init__(self, ruby_version, download_cache):
+        self.download_cache = download_cache
+        self.ruby_version = ruby_version
+        self.do = partial(self, self.ruby_version, 'do')
 
-run_with_rails = partial(rvm_do, env=RAILS_ENV)
+        self.gem = partial(self.do, 'gem')
+        self.gem_list = partial(self.gem, 'list', stdout=PIPE)
+        self.with_rails = partial(self.do, env=RAILS_ENV)
+
+    def __call__(self, *args, **kwargs):
+        cmd = 'rvm {0}'.format(' '.join(args))
+        kwargs['shell'] = True
+        return run(cmd, **kwargs)
+
+    def gem_unpack(self, name, version, unpack_dir):
+        gem_id = '{0}-{1}'.format(name, version)
+        gem_file = '{0}.gem'.format(gem_id)
+        local_gem = os.path.join(self.download_cache, gem_file)
+        if not os.path.exists(local_gem):
+            print('Downloading the {0} gem...'.format(gem_id))
+            self.gem('fetch', name, '-v', version, cwd=self.download_cache)
+        self.gem('unpack', local_gem, cwd=unpack_dir)
+
+    def gem_install(self, *gem_args, **kwargs):
+        cmd = ['install', '--no-ri', '--no-rdoc'] + list(gem_args)
+        return self.gem(*cmd, **kwargs)
+
+    def check_and_install(self, rubygems_version):
+        '''Install the correct RVM ruby and rubygems versions.'''
+        installed_rubies = self('list', 'strings', stdout=PIPE)
+        if self.ruby_version in installed_rubies:
+            print('Using Ruby {0}...'.format(self.ruby_version))
+        else:
+            print('Installing Ruby {0}...'.format(self.ruby_version))
+            with remove_installed_global_gems():
+                self('install', self.ruby_version)
+                self.do('rvm', 'rubygems', rubygems_version)
 
 
 @contextmanager
@@ -165,139 +211,149 @@ def replace_in_file(search, replace, filename):
 
 
 @contextmanager
-def code_coverage():
-    if RUN_COVERAGE and coverage:
+def code_coverage(args):
+    if args.run_coverage and coverage:
         print("Code coverage enabled.")
         cov = coverage(config_file=os.path.join(BASE_DIR, '.coveragerc'))
         cov.start()
 
     yield
 
-    if RUN_COVERAGE and coverage:
+    if args.run_coverage and coverage:
         cov.stop()
         cov.report()
 
-# Logic
 
-## flake8
-flake8 = get_style_guide(exclude=['.tox', 'build'])
-report = flake8.check_files([BASE_DIR])
+def run_flake8():
+    flake8 = get_style_guide(exclude=['.tox', 'build'])
+    report = flake8.check_files([BASE_DIR])
 
-exit_code = print_report(report, flake8)
-if exit_code > 0:
-    sys.exit(exit_code)
+    return print_report(report, flake8)
 
-if RVM_NOT_SET:
-    # Set the correct RVM ruby (1.8.7) if RVM is installed
-    installed_rubies = rvm('list', 'strings', stdout=PIPE)
-    if ruby_version in installed_rubies:
-        print('Using Ruby {0}...'.format(ruby_version))
-    else:
-        print('Installing Ruby {0}...'.format(ruby_version))
-        with remove_installed_global_gems():
-            rvm('install', ruby_version)
-            rvm_do('rvm', 'rubygems', RM_DEPS[RM_VERSION]['rubygems'])
 
-if RM_SETUP_NEEDED:
-    if os.path.isdir(LOCAL_RM_DIR):
-        print('Re-creating Redmine install...')
-        rmtree(LOCAL_RM_DIR)
+def main(argv):
+    args = parse_args(argv[0], argv[1:])
 
-    if RM_DOWNLOAD_METHOD == 'SVN':
-        run('svn', 'co', RM_SVN_URL, LOCAL_RM_DIR)
-    else:
-        if not os.path.exists(LOCAL_RM_TGZ):
-            print('Downloading Redmine {0}...'.format(RM_VERSION))
-            run('wget', '-O', LOCAL_RM_TGZ, RM_TGZ_URL)
-        run('tar', '-C', UNZIP_DIR, '-xf', LOCAL_RM_TGZ)
-    if 'sqlite3' not in gem_list():
-        gem_install('sqlite3')
+    dep_versions = RM_DEPS[args.redmine_version]
+    rvm = RVM(dep_versions['ruby'], args.download_cache)
 
-    os.chdir(LOCAL_RM_DIR)
-    # enable the REST API
-    replace_in_file(r'(rest_api_enabled:\s+default:) 0', r'\1 1',
-                    os.path.join(LOCAL_RM_DIR, 'config/settings.yml'))
-    copy(os.path.join(TEST_DIR, 'database.yml'),
-         os.path.join(LOCAL_RM_DIR, 'config'))
+    redmine_dirname = 'redmine-{0}'.format(args.redmine_version)
+    local_redmine_dir = os.path.join(args.workspace, redmine_dirname)
 
-    if RM_DOWNLOAD_METHOD == 'SVN':
-        # if redmine comes from SVN, install rails too
-        gem_install('rails', '-v', RM_DEPS[RM_VERSION]['rails'])
-        gem('list')
-    else:
-        gem_unpack('rack', RM_DEPS[RM_VERSION]['rack'],
-                   os.path.join(LOCAL_RM_DIR, 'vendor/gems'))
-    if 'rake' in gem_list():
-        gem('uninstall', '-a', '-x', 'rake')
-    gem_install('rake', '-v', '0.8.7')
-    replace_in_file('rake/rdoctask', 'rdoc/task',
-                    os.path.join(LOCAL_RM_DIR, 'Rakefile'))
-    gem_install('rdoc')
-    if 'i18n' in RM_DEPS[RM_VERSION]:
-        gem_install('i18n', '-v', '0.4.2')
+    if args.flake8:
+        exit_code = run_flake8()
+        if exit_code > 0:
+            return exit_code
 
-    prod_rb = os.path.join(LOCAL_RM_DIR, 'config/environments/production.rb')
-    session_data = '''
+    if args.check_rvm_ruby:
+        rvm.check_and_install(dep_versions['rubygems'])
+
+    if args.redmine_setup_needed:
+        if os.path.isdir(local_redmine_dir):
+            print('Re-creating Redmine install...')
+            rmtree(local_redmine_dir)
+
+        if args.redmine_download_method == 'SVN':
+            svn_checkout(args.redmine_version, local_redmine_dir)
+        else:
+            unpack_tarball(args, redmine_dirname)
+        if 'sqlite3' not in rvm.gem_list():
+            rvm.gem_install('sqlite3')
+
+        os.chdir(local_redmine_dir)
+        # Enable the REST API via the settings YAML file.
+        replace_in_file(r'(rest_api_enabled:\s+default:) 0', r'\1 1',
+                        os.path.join(local_redmine_dir, 'config/settings.yml'))
+        copy(os.path.join(TEST_DIR, 'database.yml'),
+             os.path.join(local_redmine_dir, 'config'))
+
+        if args.redmine_download_method == 'SVN':
+            # If Redmine comes from SVN, install Rails too.
+            rvm.gem_install('rails', '-v', dep_versions['rails'])
+            rvm.gem('list')
+        else:
+            rvm.gem_unpack('rack', dep_versions['rack'],
+                           os.path.join(local_redmine_dir, 'vendor/gems'))
+        if 'rake' in rvm.gem_list():
+            rvm.gem('uninstall', '-a', '-x', 'rake')
+        rvm.gem_install('rake', '-v', '0.8.7')
+        replace_in_file('rake/rdoctask', 'rdoc/task',
+                        os.path.join(local_redmine_dir, 'Rakefile'))
+        rvm.gem_install('rdoc')
+        if 'i18n' in dep_versions:
+            rvm.gem_install('i18n', '-v', dep_versions['i18n'])
+
+        prod_rb = os.path.join(local_redmine_dir,
+                               'config/environments/production.rb')
+        # TODO convert into append_to_file
+        session_data = '''
 config.action_controller.session = {
   :key => "_redmine_session",
   :secret => "31ea0a98608815189ee8118e6d6bbcbb",
 }'''
-    with open(prod_rb, 'a') as f:
-        f.write(session_data)
+        with open(prod_rb, 'a') as f:
+            f.write(session_data)
 
-    run_with_rails('rake', 'generate_session_store', 'db:migrate',
-                   'db:fixtures:load')
-    test_data = b'''
+        rvm.with_rails('rake', 'generate_session_store', 'db:migrate',
+                       'db:fixtures:load')
+        test_data = b'''
 Token.create!(:action => "api", :user_id => 2, :value => "1234abcd")
 Issue.create!(:parent_issue_id => 1, :subject => "parent issue test",
               :tracker_id => 1, :project_id => 1,
               :description => "Parent issue test", :author_id => 3)
 '''
-    with NamedTemporaryFile() as f:
-        f.write(test_data)
-        f.flush()
-        run_with_rails('script/runner', f.name)
-else:
-    os.chdir(LOCAL_RM_DIR)
+        with NamedTemporaryFile() as f:
+            f.write(test_data)
+            f.flush()
+            rvm.with_rails('script/runner', f.name)
+    else:
+        os.chdir(local_redmine_dir)
 
-pid = run_with_rails('script/server', '--daemon', '--binding=127.0.0.1',
-                     '--port={0}'.format(RM_PORT))
-# make sure the stupid server is ready to accept connections before
-# running tests
-print('Waiting for Redmine to be responsive...')
-redmine_responsive = False
-while not redmine_responsive:
-    try:
-        r = requests.head('http://127.0.0.1:{0}'.format(RM_PORT))
-        if r.status_code == 200:
-            redmine_responsive = True
-    except Exception:
-        pass
-    print('.', end='')
-    time.sleep(1)
-print()
+    # TODO convert into contextmanager
+    pid = rvm.with_rails('script/server', '--daemon', '--binding=127.0.0.1',
+                         '--port={0}'.format(RM_PORT))
+    # make sure the stupid server is ready to accept connections before
+    # running tests
+    print('Waiting for Redmine to be responsive...')
+    redmine_responsive = False
+    while not redmine_responsive:
+        try:
+            r = requests.head('http://127.0.0.1:{0}'.format(RM_PORT))
+            if r.status_code == 200:
+                redmine_responsive = True
+        except Exception:
+            pass
+        print('.', end='')
+        time.sleep(1)
+    print()
 
-print()
-print('Starting tests...')
-print()
+    print()
+    print('Starting tests...')
+    print()
 
-sys.path.append(TEST_DIR)
-os.environ['SNAKEMINE_SETTINGS_MODULE'] = 'test_settings'
-if 'REDMINE_PORT' not in os.environ:
-    os.environ['REDMINE_PORT'] = str(RM_PORT)
+    sys.path.append(TEST_DIR)
+    os.environ['SNAKEMINE_SETTINGS_MODULE'] = 'test_settings'
+    if 'REDMINE_PORT' not in os.environ:
+        os.environ['REDMINE_PORT'] = str(RM_PORT)
 
-os.chdir(BASE_DIR)
-with code_coverage():
-    test_suite = unittest.defaultTestLoader.discover(BASE_DIR)
-    runner = unittest.TextTestRunner()
-    result = runner.run(test_suite)
+    os.chdir(BASE_DIR)
+    with code_coverage(args):
+        test_suite = unittest.defaultTestLoader.discover(BASE_DIR)
+        runner = unittest.TextTestRunner()
+        result = runner.run(test_suite)
 
-pidfile = os.path.join(LOCAL_RM_DIR, 'tmp/pids/server.pid')
-if os.path.exists(pidfile):
-    pid = int(open(pidfile).read().strip())
-    os.remove(pidfile)
+    pidfile = os.path.join(local_redmine_dir, 'tmp/pids/server.pid')
+    if os.path.exists(pidfile):
+        pid = int(open(pidfile).read().strip())
+        os.remove(pidfile)
 
-os.kill(pid, SIGKILL)
+    # TODO convert into contextmanager
+    os.kill(pid, SIGKILL)
 
-if not result.wasSuccessful():
-    sys.exit(1)
+    if not result.wasSuccessful():
+        return 1
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
